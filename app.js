0a813a10c6a7251c90f09e5f3cbebb2aa3805f13
jkomoefey23e@yahoo.co.uk <jkomoefey23e@yahoo.co.uk>
2022-07-29 14:38:27 +0100

    calculated interest in progress

--- app.js
+++ app.js
@@ -4,5 +4,22 @@ document.getElementById('loan-form').addEventListener('submit', calculateResults
 // Calculate Results
 function calculateResults(e) {
     console.log('Calculating...');
+    // UI Vars
+    const amount = document.getElementById('amount');
+    const interest = document.getElementById('interest');
+    const years = document.getElementById('years');
+    const monthlyPayment = document.getElementById('monthly-payment');
+    const totalPayment = document.getElementById('total-payment');
+    const totalInterest = document.getElementById('total-interest');
+
+    const principal = parseFloat(amount.value);
+    const calculatedInterest = parseFloat(interest.value) / 100 / 12;
+    const calculatedPayments = parseFloat(years.value) * 12;
+
+    // Compute monthly payment
+    const x = Math.pow(1 + calculatedInterest, calculatedPayments);
+    const monthly = (principal * x * calculatedInterest) / (x - 1);
+
+
     e.preventDefault();
 }
