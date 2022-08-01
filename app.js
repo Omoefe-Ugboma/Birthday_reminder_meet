38fac66f3e11ad701de11d9329c6e265e18f2ffc
jkomoefey23e@yahoo.co.uk <jkomoefey23e@yahoo.co.uk>
2022-08-01 19:44:31 +0100

    Interest Calculation application Completed

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ document.getElementById('loan-form').addEventListener('submit', function (e) {
     // Show loader
     document.getElementById('loading').style.display = 'block';
 
+    setTimeout(calculateResults, 2000);
+
     e.preventDefault();
 });
 
@@ -33,6 +35,11 @@ function calculateResults() {
         monthlyPayment.value = monthly.toFixed(2);
         totalPayment.value = (monthly * calculatedPayments).toFixed(2);
         totalInterest.value = ((monthly * calculatedPayments) - principal).toFixed(2);
+
+        // Show results
+        document.getElementById('results').style.display = 'block';
+        // Hide loader
+        document.getElementById('loading').style.display = 'none';
     } else {
         showError('Please check your numbers');
         // console.log('Please check your numbers');
@@ -41,6 +48,11 @@ function calculateResults() {
 
 // Show Error
 function showError(error) {
+    // Hide results
+    document.getElementById('results').style.display = 'none';
+    // Hide loader
+    document.getElementById('loading').style.display = 'none';
+
     //  Create a div
     const errorDiv = document.createElement('div');
 
