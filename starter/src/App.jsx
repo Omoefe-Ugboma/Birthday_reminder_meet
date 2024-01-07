bbede86c32b5204eb3b0fe0dbeb5fa430831625e
jkomoefey23e@yahoo.co.uk <jkomoefey23e@yahoo.co.uk>
2024-01-07 13:17:27 +0100

    New Update

--- starter/src/App.jsx
+++ starter/src/App.jsx
@@ -1,4 +1,22 @@
+import { useState } from "react";
+import data from './data';
+import List from "./List";
+
 const App = () => {
-  return <h2>Birthday Reminder - Starter</h2>;
+ const [people,setPeople] = useState(data);
+ 
+  return <main>
+      <section className="container">
+        <h3>{people.length} birthdays today</h3>
+        <List people={people}/>
+        <button
+         type="button"
+         className="btn btn-block"
+         onClick={()=>setPeople([])}
+        >
+          clear all
+        </button>
+      </section>
+  </main>;
 };
 export default App;
